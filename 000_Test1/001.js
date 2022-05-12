9a126aa81528d12476749258859667769fe161a7
Dustin <jiangchao_mm@163.com>
2022-05-12 23:34:39 +0800

    test

---
+++ 000_Test1/001.js
@@ -0,0 +1,18 @@
+const expect = require("chai").expect
+
+
+function add(a, b) {
+    return a + b
+}
+
+describe("TS_01_TC01", function () {
+    it("step 01", function () {
+        expect(add(1, 1)).equal(2)
+    })
+})
+
+describe("TS_01_TC02", function () {
+    it("step 01", function () {
+        expect(add(1, 2)).equal(3)
+    })
+})
